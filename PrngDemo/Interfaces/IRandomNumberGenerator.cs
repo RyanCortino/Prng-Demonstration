ddca8e17f7ce4acc1afc450218ef6a6d138b6618
RyanCortino <RyanCortino@gmail.com>
2022-02-08 10:45:59 -0600

    Added a simple implementation of LCG.

--- PrngDemo/Interfaces/IRandomNumberGenerator.cs
+++ PrngDemo/Interfaces/IRandomNumberGenerator.cs
@@ -3,6 +3,9 @@
     public interface IRandomNumberGenerator
     {
         public long GetSeed();
+        public long GetLast();
         public long Next();
+        public long Next(long maxValue);
+        public string DemoResults(long maxValue, int resultsPerLine);
     }
 }
--- PrngDemo/Lcg.cs
+++ PrngDemo/Lcg.cs
@@ -4,12 +4,37 @@ namespace agRandom
 {
     public class Lcg : BasePrng
     {
-        public Lcg() : base(seed: DateTime.Now.Ticks) { }
+        private const long m = 4294967296;  // 2^32
+        private const long a = 1664525;
+        private const long c = 1012904223;
+
+        public Lcg() : base(seed: DateTime.Now.Ticks % m) { }
         public Lcg(long seed) : base(seed) { }
 
-        protected override long GenerateNext()
+        public override long Next()
         {
-            throw new NotImplementedException();
+            _last = ((a * _last) + c) % m;
+            return _last;
+        }
+
+        public override long Next(long maxValue)
+        {
+            return Next() % maxValue;
+        }
+
+        public override string DemoResults(long maxValue, int resultsPerLine)
+        {
+            string returnValue = "";
+
+            // Showing 10 results per line for easier viewing in the console window.
+            for (int j = 0; j < resultsPerLine; j++)
+            {
+                returnValue += Next(maxValue);
+                if (j < resultsPerLine--)
+                    returnValue += ", ";
+            }
+
+            return returnValue;
         }
     }
 }
